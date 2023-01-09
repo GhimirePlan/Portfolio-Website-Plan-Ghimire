b8361f37e57150f8cf0dd5c82a680cc81b500318
SajesanGhimire <90140989+SajesanGhimire@users.noreply.github.com>
2023-01-09 17:34:01 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,7 +185,50 @@ for (let i = 0; i < navigationLinks.length; i++) {
       document.getElementById("my_audio").play();
     }
 
-
+//musicplayer
+ var player = document.getElementById("player");
+      let progress = document.getElementById("progress");
+      let playbtn = document.getElementById("playbtn");
+      
+      var playpause = function () {
+        if (player.paused) {
+          player.play();
+        } else {
+          player.pause();
+        }
+      }
+      
+      playbtn.addEventListener("click", playpause);
+      
+      player.onplay = function () {
+        playbtn.classList.remove("fa-play");
+        playbtn.classList.add("fa-pause");
+      }
+      
+      player.onpause = function () {
+        playbtn.classList.add("fa-play");
+        playbtn.classList.remove("fa-pause");
+      }
+      
+      player.ontimeupdate = function () {
+        let ct = player.currentTime;
+        current.innerHTML = timeFormat(ct);
+        //progress
+        let duration = player.duration;
+        prog = Math.floor((ct * 100) / duration);
+        progress.style.setProperty("--progress", prog + "%");
+      }
+      
+      function timeFormat(ct) {
+        minutes = Math.floor(ct / 60);
+        seconds = Math.floor(ct % 60);
+      
+        if (seconds < 10) {
+          seconds = "0"+seconds;
+        }
+      
+        return minutes + ":" + seconds;
+      }
 
 
 
